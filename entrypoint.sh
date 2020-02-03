cce0e17a2c70c7db276ffc8d18534dba606a6109
lambdalisue <lambdalisue@hashnote.net>
2020-02-03 16:39:09 +0900

    Use top directory instead of container name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+AZ_CONTAINER=$1; shift
 AZ_NAME=$1; shift
 AZ_PATH=$1; shift
 
 # Create a specified container
-az storage container create -n "$AZ_NAME"
+az storage container create -n "$AZ_CONTAINER"
 
 # Upload a specified file
-az storage blob upload -c "$AZ_NAME" -n "$AZ_PATH" -f "$AZ_PATH"
+az storage blob upload -c "$AZ_CONTAINER" -n "$AZ_NAME/$AZ_PATH" -f "$AZ_PATH"
